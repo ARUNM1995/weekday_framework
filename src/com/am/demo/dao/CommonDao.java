1759a936a39e838ba5ee8c7f2b88f6082ea5e613
Arun Manjunath <arunkumar.nhce@gmail.com>
2020-03-12 20:58:02 +0530

    Update CommonDao.java

--- src/com/am/demo/dao/CommonDao.java
+++ src/com/am/demo/dao/CommonDao.java
@@ -9,15 +9,45 @@ import com.am.demo.dto.Restaurant;
 
 public class CommonDao {
 
-	
 	public void saveRestaurant(Restaurant restaurant) {
-				Configuration configuration = new Configuration();
-				configuration.configure();
-				configuration.addAnnotatedClass(Restaurant.class);
-				SessionFactory sessionFactory = configuration.buildSessionFactory();
-				Session session = sessionFactory.openSession();
-				Transaction transaction = session.beginTransaction();
-				session.save(restaurant);
-				transaction.commit();
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		configuration.addAnnotatedClass(Restaurant.class);
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(restaurant);
+		transaction.commit();
 	}
+
+	public Restaurant getRestaurantDetails(String name) {
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		configuration.addAnnotatedClass(Restaurant.class);
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Restaurant restaurant = (Restaurant) session.get(Restaurant.class, name);
+		return restaurant;
+	}
+	
+	
+	public void updateRestaurantDetails(String name,double price) {
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		configuration.addAnnotatedClass(Restaurant.class);
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Restaurant restaurant = (Restaurant) session.get(Restaurant.class, name);
+		if(restaurant!=null) {
+			restaurant.setPrice(price);
+		}else {
+			System.out.println("data does not exists");
+		}
+		session.save(restaurant);
+		transaction.commit();
+	}
+	
+	
+
 }
