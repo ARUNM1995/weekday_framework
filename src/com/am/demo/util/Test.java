044fd4c3e1219e4b277005543269c5cf56d9f231
Arun Manjunath <arunkumar.nhce@gmail.com>
2020-03-12 20:58:50 +0530

    Update Test.java

--- src/com/am/demo/util/Test.java
+++ src/com/am/demo/util/Test.java
@@ -6,17 +6,20 @@ import com.am.demo.dto.Restaurant;
 public class Test {
 
 	public static void main(String[] args) {
-
+		/*
 			Restaurant restaurant = new Restaurant();
-			restaurant.setName("udupi uphar");
-			restaurant.setContact(108);
-			restaurant.setMenu("masala dosa");
-			restaurant.setPrice(45.5);
-			restaurant.setAddress("murgeshpalya");
-			
+			restaurant.setName("leela palace");
+			restaurant.setContact(102);
+			restaurant.setMenu("buffet");
+			restaurant.setPrice(100.4);
+			restaurant.setAddress("manipals");
+			*/
 			CommonDao dao = new CommonDao();
-			dao.saveRestaurant(restaurant);
-			System.out.println("data saved");
+			dao.updateRestaurantDetails("udupi uphar", 450.5);
+			//Restaurant restaurant = dao.getRestaurantDetails("taj west end");
+			//System.out.println(restaurant);
+		//	dao.saveRestaurant(restaurant);
+			//System.out.println("data saved");
 	}
 
 }
